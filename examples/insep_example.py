2074dc84e94fee3ad3557a5a41b3870fecd40ff4
Adarsh Subbaswamy <adarsh.subbaswamy@vanderbilt.edu>
2017-08-19 06:53:44 -0400

    Inseparable example

--- examples/insep_example.py
+++ examples/insep_example.py
@@ -12,17 +12,12 @@ from lin_sep import linearly_separable
 plt.style.use('ggplot')
 
 # read linearly separable data
-data = np.loadtxt('separable.txt')
+data = np.loadtxt('insep.txt')
 
 X = data[:, 0:-1]
 y = data[:, -1]
 
 success, result = linearly_separable(X, y)
-
-h1, h2, beta = result['x']
-
-in_ = np.linspace(0, 1, 1000)
-out = beta/h2 + -h1/h2 * in_
 
 # plot
 print "Data is linearly separable: {}".format(success)
@@ -31,6 +26,5 @@ c1_inds = np.where(y == 0)[0]
 c2_inds = np.where(y == 1)[0]
 
 plt.scatter(X[:, 0], X[:, 1], c=y)
-plt.plot(in_, out)
 
-plt.savefig("sep_plot.png", dpi=300)
+plt.savefig("insep_plot.png", dpi=300)
